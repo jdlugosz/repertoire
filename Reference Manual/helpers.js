b90c276b3658855c2a1c6868e14edcfeaeb568c7
John M. Dlugosz <repertoire@dlugosz.com>
2009-05-02 00:31:22 -0500

    import history October 2, 2004 framing effects dynamic HTML

---
+++ Reference Manual/helpers.js
@@ -0,0 +1,43 @@
+
+function get_params (w, result)
+ {   
+ var url= w.document.URL;
+ var qpos= url.indexOf ('?');
+ if (qpos < 0)  return false;
+ var ppos= url.indexOf ('+', qpos+1);
+ if (ppos < 0) { // only 1 parameter given
+    result.param1= url.slice (qpos+1);
+    }
+ else {
+    result.param1= url.slice (qpos+1, ppos); // appears that 'end' parameter is EXCLUSIVE.
+    result.param2= url.slice (ppos+1);
+    }
+ return true;
+ }
+
+function is_home_url()
+ {
+ return location.hostname == "www.dlugosz.com";
+ }
+
+function checkframe (indexurl)
+ {
+ var params= new Object;
+ if (get_params (window, params)) {
+    if (params.param1 == "NOFRAMES") return;  //don't change anything.
+    else alert ("invalid parameter " + params.param1 + " ignored.");
+    }
+ if (name != "info") {
+    var infoURL= location.href;
+    // make the URL relative to this location.
+    var pos= infoURL.indexOf ('/Tomahawk/');
+    if (pos < 0)  pos= infoURL.indexOf ('/RatWin/');
+    if (pos < 0)  pos= infoURL.indexOf ('/Classics/');
+    if (pos > 0)
+       infoURL= infoURL.slice (pos+1);
+       // else just leave it the way it was.
+    var newURL= "../makeframe.html?" + infoURL + '+' + indexurl;
+    location.href= newURL;
+    }
+ }
+ 
